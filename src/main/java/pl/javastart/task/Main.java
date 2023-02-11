1adf564102b71034529cdc4512761bdfddb2bcc5
usernameMaciej <maciej.przystarz@protonmail.com>
2023-02-11 13:10:05 +0100

    poprawa

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -7,6 +7,7 @@ import java.time.temporal.TemporalAccessor;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.TimeZone;
 
 public class Main {
 
@@ -29,21 +30,33 @@ public class Main {
 
                 DateTimeFormatter ofPattern = DateTimeFormatter.ofPattern(pattern);
                 TemporalAccessor temporalAccessor = ofPattern.parse(userDataTime);
+
+
+                //Próbowałem jakoś zawięzić tekst żeby sie nie powtarzał, ale coś musiałem źle robić bo testy nie przechodziły
+                //A teraz przechodzą wszystkie
                 if (pattern.equals(patternWithoutTime)) {
                     LocalDate date = LocalDate.from(temporalAccessor);
                     LocalTime time = LocalTime.of(0, 0, 0);
-                    System.out.println("Czas lokalny: " + date + " " + time);
-                    System.out.println("UTC: " + date + " " + time.minusHours(1));
-                    System.out.println("Londyn: " + date + " " + time.minusHours(1));
-                    System.out.println("Los Angeles: " + date + " " + time.minusHours(9));
-                    System.out.println("Sydney: " + date + " " + time.plusHours(10));
+                    LocalDateTime localDateTime = LocalDateTime.of(date, time);
+                    ZonedDateTime now = ZonedDateTime.of(date, time, ZoneId.systemDefault());
+                    ZonedDateTime utc = now.withZoneSameInstant(ZoneId.of("UTC"));
+                    ZonedDateTime sydney = now.withZoneSameInstant(ZoneId.of("Australia/Sydney"));
+                    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+                    System.out.println("Czas lokalny: " + localDateTime.format(formatter));
+                    System.out.println("UTC: " + utc.format(formatter));
+                    System.out.println("Londyn: " + localDateTime.minusHours(1).format(formatter));
+                    System.out.println("Los Angeles: " + localDateTime.minusHours(9).format(formatter));
+                    System.out.println("Sydney: " + sydney.format(formatter));
                 } else {
                     LocalDateTime dateTime = LocalDateTime.from(temporalAccessor);
+                    ZonedDateTime now = ZonedDateTime.of(dateTime, ZoneId.systemDefault());
+                    ZonedDateTime utc = now.withZoneSameInstant(ZoneId.of("UTC"));
+                    ZonedDateTime sydney = now.withZoneSameInstant(ZoneId.of("Australia/Sydney"));
                     System.out.println("Czas lokalny: " + dateTime.format(formatter));
-                    System.out.println("UTC: " + dateTime.minusHours(1).format(formatter));
+                    System.out.println("UTC: " + utc.format(formatter));
                     System.out.println("Londyn: " + dateTime.minusHours(1).format(formatter));
                     System.out.println("Los Angeles: " + dateTime.minusHours(9).format(formatter));
-                    System.out.println("Sydney: " + dateTime.plusHours(10).format(formatter));
+                    System.out.println("Sydney: " + sydney.format(formatter));
                 }
             } catch (DateTimeParseException e) {
                 //ignore
